7fac21e63c1a5fb19c761e9f71750842a404802a
Reza <rezabhm50@gmail.com>
2025-05-07 12:32:48 +0330

    update slaughter ERP

--- slaughterERP/configs/urls.py
+++ slaughterERP/configs/urls.py
@@ -22,7 +22,7 @@ from rest_framework import permissions
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Slaughter ERP Title",
+      title="Slaughter ERP",
       default_version='v1',
       description="api docs for Slaughter ERP\n base url : api/v1/ ",
       terms_of_service="https://www.google.com/policies/terms/",
